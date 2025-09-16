d944be220779ba55f78b8f13562689d9c2abf8a1
Taras0310 <hoverla990310@gmail.com>
2025-09-16 13:13:50 +0300

    Resolve comments for the search_library API tests

--- clients/search_library_client.rb
+++ clients/search_library_client.rb
@@ -3,18 +3,6 @@ require 'httparty'
 class SearchLibraryClient
   BASE_URL = 'https://openlibrary.org/search.json'
 
-  def self.search_by_title(title)
-    HTTParty.get(BASE_URL, query: { q: "title:#{title}" })
-  end
-
-  def self.search_by_author(author)
-    HTTParty.get(BASE_URL, query: { q: "author:#{author}" })
-  end
-
-  def self.search_by_language(lang)
-    HTTParty.get(BASE_URL, query: { q: "language:#{lang}" })
-  end
-  
   def self.search_by_few_parametrs(**args)
     HTTParty.get(BASE_URL, query: args)
   end
@@ -23,7 +11,7 @@ class SearchLibraryClient
      HTTParty.get(BASE_URL, query: { q: search, sort: sort })
   end
   
-  def self.limit_test(search, limit)
+  def self.search_with_limit(search, limit)
      HTTParty.get(BASE_URL, query: { q: search, limit: limit })
   end       
 end 
--- spec/tests/book_api_spec.rb
+++ spec/tests/book_api_spec.rb
@@ -4,119 +4,140 @@ require_relative '../../clients/search_library_client'
 
 
 RSpec.describe 'SearchLibrary API test' do
-  it 'Check successfull connection' do
-    response = SearchLibraryClient.search_by_title('Everything is flammable')
+  it 'verifies  successfull connection' do
+    response = SearchLibraryClient.search_by_few_parametrs(
+      title: 'Everything is flammable'
+    )
     expect(response.code).to eq(200)
   end
 
   valid_titles   = ['Everything is flammable', 'EVERYTHING IS FLAMMABLE', 'everything is flammable', 'flammable']
-  invalid_titles = ['хххх', '|']
+  invalid_titles = [
+    SecureRandom.alphanumeric(10),
+    SecureRandom.alphanumeric(5),
+    'хххх', 
+    '|']
 
   valid_titles.each do |title|
-    it "Search by title returns results for '#{title}'" do
-      response = SearchLibraryClient.search_by_title(title)
+    it " verifies  search by title returns results for '#{title}'" do
+      response = SearchLibraryClient.search_by_few_parametrs(
+        title: "#{title}"
+      )
       data = JSON.parse(response.body)
-      expect(data["docs"]).not_to be_empty
+      expect(data['docs']).not_to be_empty
     end
   end
 
   invalid_titles.each do |title|
-    it "Search by title returns no results for '#{title}'" do
-      response = SearchLibraryClient.search_by_title(title)
+    it "verifies  search by title returns no results for '#{title}'" do
+      response = SearchLibraryClient.search_by_few_parametrs(
+        title: "#{title}"
+      )
       data = JSON.parse(response.body)
-      expect(data["docs"]).to be_empty
+      expect(data['docs']).to be_empty
     end
   end
 
   valid_authors   = ['Rebecca Solnit', 'REBECCA SOLNIT', 'rebecca solnit', 'solnit']
-  invalid_authors = ['хххх', '|']
+  invalid_authors = [
+    SecureRandom.alphanumeric(10),
+    SecureRandom.alphanumeric(10),
+    'хххх',
+     '|']
 
   valid_authors.each do |author|
-    it "Search by author returns results for '#{author}'" do
-      response = SearchLibraryClient.search_by_author(author)
+    it "verifies search by author returns results for '#{author}'" do
+      response = SearchLibraryClient.search_by_few_parametrs(
+        author: "#{author}")
       data = JSON.parse(response.body)
       expect(data["docs"]).not_to be_empty
     end
   end
 
   invalid_authors.each do |author|
-    it "Search by author returns no results for '#{author}'" do
-      response = SearchLibraryClient.search_by_author(author)
+    it "verifies search by author returns no results for '#{author}'" do
+      response = SearchLibraryClient.search_by_few_parametrs(
+        author: "#{author}"
+      )
       data = JSON.parse(response.body)
       expect(data["docs"]).to be_empty
     end
   end
 
   valid_langs   = ['Spa', 'SPA', 'spa', 'sp'] #tests failed
-  invalid_langs = ['хххх', '|']
+
 
   valid_langs.each do |lang|
-    it "Search by language returns results for '#{lang}'" do
-      response = SearchLibraryClient.search_by_language(lang)
+    it "verifies search by language returns results for '#{lang}'" do
+      response = SearchLibraryClient.search_by_few_parametrs(
+        language: "#{lang}"
+      )
       data = JSON.parse(response.body)
-      expect(data["docs"]).not_to be_empty
+      expect(data['docs']).not_to be_empty
     end
   end
 
-  invalid_langs.each do |lang|
-    it "Search by language returns no results for '#{lang}'" do
-      response = SearchLibraryClient.search_by_language(lang)
-      data = JSON.parse(response.body)
-      expect(data["docs"]).to be_empty
-    end
-  end
-
-  it 'Search by a few parameters with valid data' do
+  it 'verifies search by a few parameters with valid data' do
     response = SearchLibraryClient.search_by_few_parametrs(
       title: 'Everything is flammable',
       author: 'Gabrielle Bell',
       language: 'eng'
     )
     data = JSON.parse(response.body)
-    expect(data["docs"]).not_to be_empty
+    expect(data['docs']).not_to be_empty
   end
 
-  it 'Search by a few parameters with invalid data' do
+  it 'verifies search by a few parameters with invalid data' do
     response = SearchLibraryClient.search_by_few_parametrs(
-      title: 'ss',
-      author: 'kk',
-      language: 'll'
+      title: SecureRandom.alphanumeric(10),
+      author: SecureRandom.alphanumeric(10),
+      language: SecureRandom.alphanumeric(10)
     )
     data = JSON.parse(response.body)
-    expect(data["docs"]).to be_empty
+    expect(data['docs']).to be_empty
   end
 
-  it 'Response includes required fields after successful search' do
-    response = SearchLibraryClient.search_by_author('Rebecca Solnit')
+  it 'verifies  that response includes required fields after successful search' do
+    response = SearchLibraryClient.search_by_few_parametrs(
+      author: 'Rebecca Solnit'
+    )
     data = JSON.parse(response.body)
-    expect(data["docs"].first).to include("title", "author_name", "first_publish_year")
+    expect(data['docs'].first).to include('title', 'author_name', 'first_publish_year')
   end
 
-  it 'Response does not include required fields when nvalid  search' do
-    response = SearchLibraryClient.search_by_language("|")
+  it 'verifies  that response does not include required fields when nvalid  search' do
+    response = SearchLibraryClient.search_by_few_parametrs(
+      test_parametr: "|"
+    )
     data = JSON.parse(response.body)
-    expect(data["docs"]).to be_empty
+    expect(data['docs']).to be_empty
   end
 
-  it 'Sort search results with a valid sort parameter' do
-    response = SearchLibraryClient.sort_search_result("author:Rebecca Solnit", "new")
+  it 'verifies sort search results with a valid sort parameter' do
+    response = SearchLibraryClient.sort_search_result(
+      'author:Rebecca Solnit', 
+      'new'
+    )
     data = JSON.parse(response.body)
-    expect(data["docs"][-1]["first_publish_year"] < data["docs"][0]["first_publish_year"])
+    expect(data['docs'][-1]['first_publish_year'] < data['docs'][0]['first_publish_year'])
   end
 
-  it 'Sort search results with an invalid sort parameter' do
-    response = SearchLibraryClient.sort_search_result("author:Rebecca Solnit", "t")
+  it 'verifies sort search results with an invalid sort parameter' do
+    response = SearchLibraryClient.sort_search_result(
+      'author:Rebecca Solnit', 
+      SecureRandom.alphanumeric(3)
+    )
     expect(response.code).to eq(500)
   end
 
-  it 'Search with a valid limit parameter' do
-    response = SearchLibraryClient.limit_test("author:Solnit", 5)
+  it 'verifies search with a valid limit parameter' do
+    response = SearchLibraryClient.search_with_limit('author:Solnit', 5)
     data = JSON.parse(response.body)
-    expect(data["docs"].size).to be <= 5
+    expect(data['docs'].size).to be <= 5
   end
 
-  it 'Search with a limit parameter < 0' do
-    response = SearchLibraryClient.limit_test("author:Solnit", -1)
+  it 'verifies search with a limit parameter < 0' do
+    response = SearchLibraryClient.search_with_limit('author:Solnit', -1)
     expect(response.code).to eq(500)
   end
 end
